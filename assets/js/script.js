c7a4d4a84e4e6db396b9db88437534c0a53f53e6
cgar246 <christiang3268@yahoo.com>
2021-08-19 21:33:48 -0700

    addedsave feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEL = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDOEl = document.querySelector("#tasks-to-do")
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDOEl.appendChild(listItemEl);
 };
 
-buttonEL.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
